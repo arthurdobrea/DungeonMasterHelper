aff366104f108fa224a92d77e19a9409ff1ee0c8
ArthurAD <arthurdobrya@gmail.com>
2025-12-27 11:57:45 +0200

    Adding all basic features

---
+++ vite.config.ts
@@ -0,0 +1,12 @@
+import { defineConfig } from 'vite';
+// @ts-ignore
+import react from '@vitejs/plugin-react';
+// @ts-ignore
+import tailwindcss from '@tailwindcss/vite';
+
+export default defineConfig({
+  plugins: [
+    react(),
+    tailwindcss(),
+  ],
+});
